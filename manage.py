c0a80a2ce42df4115bc48daa2d254a3febfab6a6
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2022-02-05 22:49:10 +0300

    edit file

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
-from app import create_app,db
-from flask_script import Manager,Server
+from app import create_app, db
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import User, Pitches, Comments
 
@@ -7,7 +7,7 @@ app = create_app('production')
 # app = create_app('development')
 
 manager = Manager(app)
-manager.add_command('server',Server)
+manager.add_command('server', Server)
 
 
 @manager.command
@@ -17,12 +17,14 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User,Pitches = Pitches, Comments= Comments)
+    return dict(app=app, db=db, User=User, Pitches=Pitches, Comments=Comments)
 
-migrate = db(app,db)
-manager.add_command('db',migrate)
+
+migrate = db(app, db)
+manager.add_command('db', migrate)
 
 if __name__ == '__main__':
     manager.run()

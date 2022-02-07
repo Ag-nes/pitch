6aa20e1d7add0d352e89bda427a46aaae506628d
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2022-02-07 23:50:48 +0300

    run tests

---
+++ manage.py
@@ -0,0 +1,28 @@
+from app import create_app,db
+from app.models import User,Comment,Pitch,Category,Role
+from flask_migrate import Migrate
+
+
+
+
+
+app=create_app('development')
+migrate=Migrate()
+migrate.init_app(app,db)
+
+@app.cli.command()
+def test():
+  """
+  Run unit tests
+  """
+  import unittest
+
+  tests=unittest.TestLoader().discover('tests')
+  unittest.TextTestRunner(verbosity=2).run(tests)
+
+@app.shell_context_processor
+def make_shell_context():
+  return dict(app=app,db=db,User=User,Comment=Comment,Pitch=Pitch,Category=Category,Role=Role)
+
+if __name__=='__main__':
+  app.run()
